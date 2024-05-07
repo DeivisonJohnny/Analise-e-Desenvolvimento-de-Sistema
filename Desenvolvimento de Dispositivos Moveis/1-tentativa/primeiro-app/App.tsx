d7897b7d2aff9424b61885e1699184d72bab0684
Deivison Johnny <DeivisonJohnny@gmail.com>
2024-05-07 05:14:52 -0300

    adicionando materia

---
+++ Desenvolvimento de Dispositivos Moveis/1-tentativa/primeiro-app/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { LoginScreen } from "./src/screens/login";
+
+
+
+
+export default function App() {
+  return(<LoginScreen/>)
+}
