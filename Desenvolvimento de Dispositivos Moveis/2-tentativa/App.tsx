8bda1a4990361d07814490e237c1172b14f8dbb7
Deivison Johnny <DeivisonJohnny@gmail.com>
2024-05-19 02:29:00 -0300

    Praticando uso das props

--- Desenvolvimento de Dispositivos Moveis/2-tentativa/App.tsx
+++ Desenvolvimento de Dispositivos Moveis/2-tentativa/App.tsx
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Index from './src/screens';
+import {Praticando} from './src/screens/props';
+// import Index from './src/screens';
 
 export default function App() {
   return (
-    <Index/>
+    <View>
+      <Praticando text='Botão 1' colorBg='red' onPress={() => {console.log('clicou 1')}}/>
+      <Praticando text='Botão 2' colorBg='blue' onPress={() => {console.log('clicou 2')}}/>
+      <Praticando text='Botão 3' onPress={() => {console.log('clicou 3')}}/>
+      <Text>
+        
+      </Text>
+    </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    flexDirection: 'column',
+    justifyContent: 'center'
+  }
+})
