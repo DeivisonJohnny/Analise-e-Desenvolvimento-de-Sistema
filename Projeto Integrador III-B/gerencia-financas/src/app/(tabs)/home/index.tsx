0133637eae19199d5fc2cc4b960798b27739db29
Deivison Johnny <DeivisonJohnny@gmail.com>
2024-05-23 06:53:03 -0300

    criando page home

--- Projeto Integrador III-B/gerencia-financas/src/app/(tabs)/home/index.tsx
+++ Projeto Integrador III-B/gerencia-financas/src/app/(tabs)/home/index.tsx
@@ -1,3 +1,4 @@
+import { color } from '@rneui/base';
 import * as React from 'react';
 import { Text, View, StyleSheet, ImageBackground, } from 'react-native';
 
@@ -5,13 +6,33 @@ import { Text, View, StyleSheet, ImageBackground, } from 'react-native';
 
 
 const Home = () => {
-return (
-    
-    <ImageBackground source={require('../../assets/img/bg-home.png')} style={styles.body} imageStyle={{width: '100%', height: '100%'}}>
+  return (
 
-       <View style={styles.menu}>
+    <ImageBackground source={require('../../assets/img/bg-home.png')} style={styles.body} imageStyle={{ width: '100%', height: '100%' }}>
 
-       </View>
+      <View style={styles.containerSaldos}>
+
+
+        <View style={styles.boxSaldos}>
+          <Text style={[styles.titleSaldo, {color: 'red'}]}>Dispesas</Text>
+          <Text style={{ fontSize: 25, color: 'white', fontWeight: 'bold' }}>R$ 2.300,00</Text>
+          <View style={{ flexDirection: 'row', gap: 5 }}>
+            <Text>Status: </Text>
+            <Text>Insuficiente</Text>
+          </View>
+        </View>
+
+        <View style={styles.boxSaldos} >
+          <Text style={styles.titleSaldo}>Renda</Text>
+          <Text style={{ fontSize: 25, color: 'white', fontWeight: 'bold' }}>R$ 1.757,32</Text>
+          <View style={{ flexDirection: 'row', gap: 5 }}>
+            <Text>Status: </Text>
+            <Text>Insuficiente</Text>
+          </View>
+        </View>
+
+      </View>
+
     </ImageBackground>
 
   );
@@ -25,9 +46,38 @@ const styles = StyleSheet.create({
     height: '100%',
   },
 
+  containerSaldos: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    width: '100%',
+    paddingTop: 15,
+    paddingHorizontal: 15,
+  },
 
-  menu: {
 
+  boxSaldos: {
+    borderWidth: 1,
+    borderColor: '#ffffff28',
+    justifyContent: 'space-around',
+    width: '48%',
+    height: 100,
+    backgroundColor: '#ffffff0c',
+    padding: 10,
+    borderRadius: 5
+  },
+
+  titleSaldo: {
+    color: '#00cf6885',
+    fontWeight: 'bold',
+    fontSize: 12,
+    letterSpacing: 1.2
+  },
+
+
+  textStatus: {
+    color: 'gray',
+    fontSize: 12,
   }
 
 });
