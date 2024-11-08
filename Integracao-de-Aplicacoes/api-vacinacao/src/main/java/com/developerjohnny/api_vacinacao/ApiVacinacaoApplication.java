1945cfd3a9b4b11ebbe93e4173175cc99afb539b
Deivison Johnny <deivisonjohnny@gmail.com>
2024-11-08 10:29:22 -0300

    feature

---
+++ Integracao-de-Aplicacoes/api-vacinacao/src/main/java/com/developerjohnny/api_vacinacao/ApiVacinacaoApplication.java
@@ -0,0 +1,13 @@
+package com.developerjohnny.api_vacinacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVacinacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVacinacaoApplication.class, args);
+	}
+
+}
